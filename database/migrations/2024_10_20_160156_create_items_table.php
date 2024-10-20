f39020034ad3c211614c20950c0dc718e6599a05
giordanofabrizii <giordanofabrizi@gmail.com>
2024-10-20 18:04:05 +0200

    added migrations

--- database/migrations/2024_10_20_160156_create_items_table.php
+++ database/migrations/2024_10_20_160156_create_items_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->string('icon');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role');
-            $table->rememberToken();
+            $table->boolean('available')->default(false);
+            $table->foreignId('rack_id')->constrained('racks')->onDelete('cascade')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('items');
     }
 };
