f3b70b9c8f982cac20d6c09cfdd0687ac01bb79c
giordanofabrizii <giordanofabrizi@gmail.com>
2024-10-28 19:23:53 +0100

    started style

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,101 +5,68 @@
 @endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <div class="myContainer">
+        <form method="POST" action="{{ route('register') }}" id="myForm">
+            @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}" id="myForm">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" name="name" value="{{ old('name') }}" autocomplete="name" autofocus placeholder="Es: Mario Rossi">
-
-                                <p class="error"></p>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" name="email" value="{{ old('email') }}" autocomplete="email" placeholder="Es: mario@email.it">
-
-                                <p class="error"></p>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="code" class="col-md-4 col-form-label text-md-end">{{ __('Codice scuola') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="code" type="code" name="code" value="{{ old('code') }}" autocomplete="code" placeholder="Es: XXXXXXXX">
-
-                                <p class="error"></p>
-
-                                @error('code')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" name="password" type="password" autocomplete="new-password">
-
-                                <p class="error"></p>
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" name="password_confirmation" type="password" autocomplete="new-password">
-
-                                <p class="error"></p>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+            <div class="inputContainer">
+                <label for="name">Nome</label>
+                <div>
+                    <input id="name" type="text" name="name" value="{{ old('name') }}" autocomplete="name" autofocus placeholder="Es: Mario Rossi">
+                    <p class="error"></p>
+                    @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
             </div>
-        </div>
+            <div class="inputContainer">
+                <label for="email">Indirizzo email</label>
+                <div>
+                    <input id="email" type="email" name="email" value="{{ old('email') }}" autocomplete="email" placeholder="Es: mario@email.it">
+                    <p class="error"></p>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+            <div class="inputContainer">
+                <label for="code">Codice scuola</label>
+                <div>
+                    <input id="code" type="text" name="code" value="{{ old('code') }}" autocomplete="code" placeholder="Es: XXXXXXXX">
+                    <p class="error"></p>
+                    @error('code')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+            <div class="inputContainer">
+                <label for="password">Password</label>
+                <div>
+                    <input id="password" name="password" type="password" autocomplete="new-password">
+                    <p class="error"></p>
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+            <div class="inputContainer">
+                <label for="password-confirm">Conferma password</label>
+                <div>
+                    <input id="password-confirm" name="password_confirmation" type="password" autocomplete="new-password">
+                    <p class="error"></p>
+                </div>
+            </div>
+                <div>
+                    <button type="submit">Registrati</button>
+                </div>
+        </form>
     </div>
-</div>
 @endsection
