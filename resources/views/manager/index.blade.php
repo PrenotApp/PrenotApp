20d8833bdc3e5428acf3438b42040a82b11726e7
MirkoValle <mirko.valle20@gmail.com>
2024-11-04 18:56:07 +0100

    Added style for manager

--- resources/views/manager/index.blade.php
+++ resources/views/manager/index.blade.php
@@ -1,39 +1,58 @@
 @extends('layouts.app')
 
+@section('custom-scss')
+    @vite(['resources/sass/manager/index.scss'])
+@endsection
+
 @section('links')
     @vite(['resources/js/validations/schoolCreate.js'])
 @endsection
 
 
 @section('content')
-@if (session('success'))
-<div class="alert alert-success">
-    {{ session('success') }}
-</div>
-@endif
 
-    <a href="{{ route('manager.trashed') }}">
-        Cestino
-    </a>
+    @if (session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+    @endif
 
-    <form action="{{ route('manager.store') }}" method="POST" id="myForm">
-        @csrf
+    <section id="index">
 
-        <label for="name">Nome</label>
-        <input type="text" name="name" id="name" placeholder="Scrivi il nome della scuola">
+        <div class="button">
+            <a href="{{ route('manager.trashed') }}">
+                Cestino
+            </a>
+        </div>
 
-        <button type="submit">Aggiungi</button>
-    </form>
+        <form class="inputContainer top" action="{{ route('manager.store') }}" method="POST" id="myForm">
+            @csrf
+            <div>
+                <label for="name">Nome</label>
+                <input type="text" name="name" id="name" placeholder="Scrivi il nome della scuola">
+            </div>
 
-    @foreach($schools as $school)
-        @if ($school->code !== 'ADM908IN')
-            <h2>{{ $school->name }} - <span>Codice: {{ $school->code }}</span></h2>
-            <form action="{{ route('manager.delete', $school) }}" method="POST">
-                @method('DELETE')
-                @csrf
+            <button class="submit" type="submit">Aggiungi</button>
+        </form>
 
-                <button>Elimina</button>
-            </form>
-        @endif
-    @endforeach
+        <section class="list">
+            @foreach($schools as $school)
+                @if ($school->code !== 'ADM908IN')
+                    <div class="school">
+                        <div>
+                            <h2>Nome: {{ $school->name }}</h2>
+                            <h2>Codice: {{ $school->code }}</h2>
+                        </div>
+                        <form action="{{ route('manager.delete', $school) }}" method="POST">
+                            @method('DELETE')
+                            @csrf
+
+                            <button class="submit">Elimina</button>
+                        </form>
+                    </div>
+                    <hr>
+                @endif
+            @endforeach
+        </div>
+    </section>
 @endsection
