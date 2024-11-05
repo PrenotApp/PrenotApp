adecdc91c40c49785ddbd94efdeed61970f64fc6
giordanofabrizii <giordanofabrizi@gmail.com>
2024-11-05 13:51:15 +0100

    added small improvments

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -1,25 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
+    @if (session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+    @endif
+    @if (session('error'))
+    <div class="alert alert-danger">
+        {{ session('error') }}
+    </div>
+    @endif
 
-@if (session('success'))
-<div class="alert alert-success">
-    {{ session('success') }}
-</div>
-@endif
+    @if(Auth::check() && Auth::user()->role !== 'common')
 
-@if(Auth::check() && Auth::user()->role !== 'common')
-
-    <a href="{{ route('item.create') }}">
-        Aggiungi dispositivo
-    </a>
-    <a href="{{ route('category.create') }}">
-        Aggiungi categoria
-    </a>
-    <a href="{{ route('rack.create') }}">
-        Aggiungi gruppo
-    </a>
-@endif
+        <a href="{{ route('item.create') }}">
+            Aggiungi dispositivo
+        </a>
+        <a href="{{ route('category.create') }}">
+            Aggiungi categoria
+        </a>
+        <a href="{{ route('rack.create') }}">
+            Aggiungi gruppo
+        </a>
+    @endif
 
     @foreach ($groupedItems as $category => $items)
         <h2><i class="{{ $items[0]->category_icon }}"></i>{{ $category }}</h2>
@@ -36,5 +40,13 @@
 
     @foreach($racks as $rack)
         <h2>{{ $rack->name }} <a href="{{ route('rack.edit',$rack) }}">Modifica</a><a href="{{ route('rack.booking',$rack->id) }}">Prenota</a></h2>
+        <ul>
+            @php
+                $rackItems = $rack->items
+            @endphp
+            @foreach($rackItems as $rackItem)
+                <li>{{$rackItem->name}}</li>
+            @endforeach
+        </ul>
     @endforeach
 @endsection
