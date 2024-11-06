2580999463225be49d63f98dd8354d8c220977a3
giordanofabrizii <giordanofabrizi@gmail.com>
2024-11-06 11:52:53 +0100

    small css edit

--- resources/views/bookings/index.blade.php
+++ resources/views/bookings/index.blade.php
@@ -5,27 +5,29 @@
 @endsection
 
 @section('content')
-    <h1>Lista Prenotazioni</h1>
+    <section class="main-container">
+        <div class="content-contaioner">
+            <h1>Lista Prenotazioni</h1>
+            <form id="filterForm">
+                @csrf
 
-    <form id="filterForm">
-        @csrf
-
-        <div class="row">
-            <div class="col-md-3">
-                <label for="start_date">Da:</label>
-                <input type="date" name="start_date" id="start_date" class="form-control">
-            </div>
-            <div class="col-md-3">
-                <label for="end_date">A:</label>
-                <input type="date" name="end_date" id="end_date" class="form-control">
-            </div>
-            <div class="col-md-2">
-                <button type="submit" class="btn btn-primary mt-4">Filtra</button>
+                <div class="row">
+                    <div class="col-md-3">
+                        <label for="start_date">Da:</label>
+                        <input type="date" name="start_date" id="start_date" class="form-control">
+                    </div>
+                    <div class="col-md-3">
+                        <label for="end_date">A:</label>
+                        <input type="date" name="end_date" id="end_date" class="form-control">
+                    </div>
+                    <div class="col-md-2">
+                        <button type="submit" class="btn btn-primary mt-4">Filtra</button>
+                    </div>
+                </div>
+            </form>
+            <div id="bookingsList">
+                @include('bookings.partials.list', ['bookings' => $bookings])
             </div>
         </div>
-    </form>
-
-    <div id="bookingsList">
-        @include('bookings.partials.list', ['bookings' => $bookings])
-    </div>
+    </section>
 @endsection
--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -30,108 +30,102 @@
     </section>
     @endif
 
-<section class="main-container" id="index">
-
-
-
-    <div class="accordion content-container" id="accordionCategories">
-        @foreach ($groupedItems as $category => $items)
-        <div class="accordion-item mb-4">
-            <h2 class="accordion-header" id="heading-{{ Str::slug($category) }}">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-{{ Str::slug($category) }}" aria-expanded="false" aria-controls="collapse-{{ Str::slug($category) }}">
-                    <i class="{{ $items[0]->category_icon }}"></i><span>{{ $category }} ({{ count($items) }})</span>
-                </button>
-            </h2>
-            <div id="collapse-{{ Str::slug($category) }}" class="accordion-collapse collapse" aria-labelledby="heading-{{ Str::slug($category) }}">
-                <div class="accordion-body">
-                    <ul>
-                        @foreach ($items as $index => $item)
-                        <li>
-                            <a href="{{ route('item.show', $item->id) }}">{{ $item->name }}</a>
-                            <div>
-                                @if(Auth::check() && Auth::user()->role !== 'common')
-                                <a class="btn btn-warning" href="{{ route('item.edit', $item->id) }}">Modifica</a>
-                                @endif
-                                <a class="btn btn-primary" href="{{ route('booking.create', $item->id) }}">Prenota</a>
-                            </div>
-                        </li>
-                        @if ($index < count($items) - 1)
-                            <hr>
-                        @endif
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
-        </div>
-        @endforeach
-    </div>
-
-    <div class="accordion content-container" id="accordionCarelli">
-        <div class="accordion-item">
-            <h2 class="accordion-header">
-                <div class="accordion-button last-accordion">
-                    <h3><span>Carrelli</span>
-                        <span class="d-inline-block" tabindex="0" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-content="Clicca sul nome del carrello per visualizzare tutti i dipositivi che appartengono ad esso" data-bs-custom-class="custom-popover">
-                            <div class="help-container" disabled><i class="fa-solid fa-question help"></i></div>
-                        </span>
-                    </h3>
-                    @if(Auth::check() && Auth::user()->role !== 'common')
-                    <a class="btn btn-add" href="{{ route('rack.create') }}"><i class="fa-solid fa-plus"></i></a>
-                    @endif
-                </div>
-            </h2>
-            <div id="panelsStayOpen-collapseCarelli" class="accordion-collapse collapse show">
-                <div class="accordion-body">
-                    <div class="accordion" id="accordionRacks">
-                        @foreach($racks as $rack)
-                            <div>
-                                <h2 id="headingRack-{{ $rack->id }}">
-                                    <section class="rack-section">
-                                        <h4 data-bs-toggle="collapse" data-bs-target="#collapseRack-{{ $rack->id }}" aria-expanded="false" aria-controls="collapseRack-{{ $rack->id }}">
-                                            {{ $rack->name }}
-                                        </h4>
-                                        <div>
-                                            @if(Auth::check() && Auth::user()->role !== 'common')
-                                            <a class="btn btn-warning" href="{{ route('rack.edit', $rack) }}">Modifica</a>
-                                            @endif
-                                            <a class="btn btn-primary" href="{{ route('rack.booking', $rack->id) }}">Prenota</a>
-                                        </div>
-                                    </section>
-                                </h2>
-
-                                <div id="collapseRack-{{ $rack->id }}" class="accordion-collapse collapse" aria-labelledby="headingRack-{{ $rack->id }}">
-                                    <div class="accordion-body last-body">
-                                        <ul>
-                                            @foreach ($groupedItems as $items)
-                                                @php
-                                                    // Filtra solo gli items che appartengono al rack corrente
-                                                    $filteredItems = array_filter($items, function($item) use ($rack) {
-                                                        return $item->rack_id === $rack->id;
-                                                    });
-                                                @endphp
-
-                                                @foreach ($filteredItems as $item)
-                                                    <li>
-                                                        <p class="racks-item"><span>&#9679;</span>{{ $item->name }}</p>
-                                                    </li>
-                                                @endforeach
-                                            @endforeach
-                                        </ul>
-                                    </div>
+    <section class="main-container" id="index">
+        <div class="accordion content-container" id="accordionCategories">
+            @foreach ($groupedItems as $category => $items)
+            <div class="accordion-item mb-4">
+                <h2 class="accordion-header" id="heading-{{ Str::slug($category) }}">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-{{ Str::slug($category) }}" aria-expanded="false" aria-controls="collapse-{{ Str::slug($category) }}">
+                        <i class="{{ $items[0]->category_icon }}"></i><span>{{ $category }} ({{ count($items) }})</span>
+                    </button>
+                </h2>
+                <div id="collapse-{{ Str::slug($category) }}" class="accordion-collapse collapse" aria-labelledby="heading-{{ Str::slug($category) }}">
+                    <div class="accordion-body">
+                        <ul>
+                            @foreach ($items as $index => $item)
+                            <li>
+                                <a href="{{ route('item.show', $item->id) }}">{{ $item->name }}</a>
+                                <div>
+                                    @if(Auth::check() && Auth::user()->role !== 'common')
+                                    <a class="btn btn-warning" href="{{ route('item.edit', $item->id) }}">Modifica</a>
+                                    @endif
+                                    <a class="btn btn-primary" href="{{ route('booking.create', $item->id) }}">Prenota</a>
                                 </div>
-                            </div>
-                            @if (!$loop->last)
+                            </li>
+                            @if ($index < count($items) - 1)
                                 <hr>
                             @endif
-                        @endforeach
+                            @endforeach
+                        </ul>
                     </div>
                 </div>
             </div>
-
+            @endforeach
         </div>
-    </div>
 
+        <div class="accordion content-container" id="accordionCarelli">
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <div class="accordion-button last-accordion">
+                        <h3><span>Carrelli</span>
+                            <span class="d-inline-block" tabindex="0" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-content="Clicca sul nome del carrello per visualizzare tutti i dipositivi che appartengono ad esso" data-bs-custom-class="custom-popover">
+                                <div class="help-container" disabled><i class="fa-solid fa-question help"></i></div>
+                            </span>
+                        </h3>
+                        @if(Auth::check() && Auth::user()->role !== 'common')
+                        <a class="btn btn-add" href="{{ route('rack.create') }}"><i class="fa-solid fa-plus"></i></a>
+                        @endif
+                    </div>
+                </h2>
+                <div id="panelsStayOpen-collapseCarelli" class="accordion-collapse collapse show">
+                    <div class="accordion-body">
+                        <div class="accordion" id="accordionRacks">
+                            @foreach($racks as $rack)
+                                <div>
+                                    <h2 id="headingRack-{{ $rack->id }}">
+                                        <section class="rack-section">
+                                            <h4 data-bs-toggle="collapse" data-bs-target="#collapseRack-{{ $rack->id }}" aria-expanded="false" aria-controls="collapseRack-{{ $rack->id }}">
+                                                {{ $rack->name }}
+                                            </h4>
+                                            <div>
+                                                @if(Auth::check() && Auth::user()->role !== 'common')
+                                                <a class="btn btn-warning" href="{{ route('rack.edit', $rack) }}">Modifica</a>
+                                                @endif
+                                                <a class="btn btn-primary" href="{{ route('rack.booking', $rack->id) }}">Prenota</a>
+                                            </div>
+                                        </section>
+                                    </h2>
 
+                                    <div id="collapseRack-{{ $rack->id }}" class="accordion-collapse collapse" aria-labelledby="headingRack-{{ $rack->id }}">
+                                        <div class="accordion-body last-body">
+                                            <ul>
+                                                @foreach ($groupedItems as $items)
+                                                    @php
+                                                        // Filtra solo gli items che appartengono al rack corrente
+                                                        $filteredItems = array_filter($items, function($item) use ($rack) {
+                                                            return $item->rack_id === $rack->id;
+                                                        });
+                                                    @endphp
 
-</section>
+                                                    @foreach ($filteredItems as $item)
+                                                        <li>
+                                                            <p class="racks-item"><span>&#9679;</span>{{ $item->name }}</p>
+                                                        </li>
+                                                    @endforeach
+                                                @endforeach
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                @if (!$loop->last)
+                                    <hr>
+                                @endif
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
 @endsection
