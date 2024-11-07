931e6a38979bd287a05e152d2acf5de67cce7e29
giordanofabrizii <giordanofabrizi@gmail.com>
2024-11-07 13:22:43 +0100

    setted email functions

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,8 +8,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\School;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Mail;
-use App\Mail\VerifyEmail;
 use App\Models\Category;
 use Illuminate\Support\Facades\Config;
 use App\Models\Rack;
@@ -40,11 +38,6 @@ class HomeController extends Controller
         $categories = Category::where('school_id', $user->school_id)->get();
         $racks = Rack::where('school_id', $user->school_id)->get();
 
-        // Invia l'email con il codice di verifica
-        $verificationCode = rand(100000, 999999); // Codice di verifica generato (esempio con 6 cifre)
-        // Mail::to('giordanofabrizi@gmail.com')->send(new VerifyEmail($verificationCode, $user->name));
-
-
-        return view('main.index', compact('user','categories','racks', 'school'));
+        return view('main.index', compact('user','categories','racks'));
     }
 }
