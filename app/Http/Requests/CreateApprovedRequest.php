2efff65b312ba0d5e9a7b132818e59e2ac913c7a
MirkoValle <mirko.valle20@gmail.com>
2024-11-09 13:39:55 +0100

    Added unique on approved

--- app/Http/Requests/CreateApprovedRequest.php
+++ app/Http/Requests/CreateApprovedRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class CreateApprovedRequest extends FormRequest
 {
@@ -21,9 +22,18 @@ class CreateApprovedRequest extends FormRequest
      */
     public function rules(): array
     {
+
         return [
-            'email' => 'required|string|max:255',
+            'email' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('approveds', 'email')
+                    ->where('school_id', auth()->user()->school_id)
+                    ->ignore($this->route('approved')), // Ignora l'ID corrente per aggiornamenti
+            ],
             'school_id' => 'nullable'
         ];
+
     }
 }
